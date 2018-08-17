9d23ba5faee1fe1defb26cc3c24f40e8d3b5787b
ykiwmy <1293089043@qq.com>
2018-08-17 08:25:15 +0800

    0817

--- ilktest-17/ILKTest/WcuComm.h
+++ ilktest-17/ILKTest/WcuComm.h
@@ -5,13 +5,24 @@ class CADDevice;
 
 class WcuComm
 {
+	
+
 public:
 	WcuComm::WcuComm(CString deviceName,CString relayName, CADDevice* pDevice);
 	~WcuComm();
 
 public:
+	enum WCU_STAT
+	{
+		COMM_LOW, COMM_HIGH, COMM_NEIGHER, COMM_BOTH,
+	};
 	CString deviceName_;	// Éè±¸Ãû³Æ
 	CString name_;
 	CADDevice* pDevice_;
+	WCU_STAT	stat_;		// ×´Ì¬
+
+public:
+	BOOL isHigh(void) { return stat_ == COMM_HIGH; }  //1
+	BOOL isLow(void) { return stat_ == COMM_LOW; } //0
 };
 
